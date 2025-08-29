c48c151fa248979475d1d1c3c3f2a805dc024373
pitr <pitr.vishnyakovv.05@gmail.com>
2025-08-29 19:35:23 +0500

    orders payment before logging + db diagram + notes

--- enf/enf/settings.py
+++ enf/enf/settings.py
@@ -29,9 +29,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+
     'main',
     'cart',
     'users',
+    'orders',
+    'payment',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +138,6 @@ SESSION_COOKIE_AGE = 86400     #30 days
 SESSION_SAVE_EVERY_REQUEST = True   # == cart.py / save() - вручную
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+STRIPE_SECRET_KEY =  os.getenv('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
